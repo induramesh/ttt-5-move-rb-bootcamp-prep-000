c1f17854733d6b4d190de14d6ae3a5e904712d5d
Indu Ramesh <induramesh@gmail.com>
2018-09-12 18:29:47 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -3,8 +3,8 @@ def input_to_index(input)
   input.to_i - 1 
 end
 
-def move(board,index,value)
-  board[index] = "X"
+def move(board,index,value="X")
+  board[index] = value 
 end 
 
 def display_board(board)
